21cf916a531f76119920e2aed6afe243f189f697
Malik Irain <malik.irain@cemes.fr>
2025-05-27 11:37:56 +0200

    give a logger that prints to console when writing to file is not possible

--- src/pymodaq_utils/logger.py
+++ src/pymodaq_utils/logger.py
@@ -36,15 +36,25 @@ def set_logger(logger_name, add_handler=False, base_logger=False, add_to_console
         logger_name = f'{logger_base_name}.{logger_name}'
 
     logger = logging.getLogger(logger_name)
-    log_path = get_set_config_dir('log', user=True)
+
     if log_level is None:
         log_level = config('general', 'debug_level')
     logger.setLevel(log_level)
     if add_handler:
-        log_file_path = log_path.joinpath(f'{logger_base_name}.log')
-        if not log_file_path.is_file():
-            log_file_path.touch(mode=0o777)
-        handler = TimedRotatingFileHandler(log_file_path, when='midnight')
+        try:
+            log_path = get_set_config_dir('log', user=True)
+            log_file_path = log_path.joinpath(f'{logger_base_name}.log')
+            if not (log_file_path.exists() and log_file_path.is_file()):
+                log_file_path.touch(mode=0o777)
+            handler = TimedRotatingFileHandler(log_file_path, when='midnight')
+        except Exception as e:
+            import time
+            print(e)
+            print(f"Could not set up logger at {log_file_path}. Probably because of missing rights.")
+            print("Falling back to console logging.")
+            time.sleep(2)
+            handler = logging.StreamHandler()
+
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
@@ -76,3 +86,4 @@ def get_module_name(module__file__path):
     """from the full path of a module extract its name"""
     path = Path(module__file__path)
     return path.stem
+
